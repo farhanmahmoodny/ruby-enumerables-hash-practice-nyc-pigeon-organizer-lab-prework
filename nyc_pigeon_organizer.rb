a3b267fbe5c8dc231bde288a06f5d3e9311ca36a
Farhan Mahmood <farhanmahmoodny@gmail.com>
2020-11-27 00:59:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,8 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   new = {}
   arr = data[:gender][:male].concat(data[:gender][:female])
-  
+  arr.each do |item|
+    new[item] = {}
+  end
+  new
 end
