03cdab07aac0cde853213306ce7acf4ac049fa54
Farhan Mahmood <farhanmahmoodny@gmail.com>
2020-11-27 01:13:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,10 +5,5 @@ def nyc_pigeon_organizer(data)
   arr.each do |item|
     new[item] = {:color => [], :gender => [], :lives => []}
   end
-  data[:color].each do |color|
-    color.each do |pigeon|
-      new[pigeon][:color].push(color)
-    end
-  end
   new
 end
